351e3cdc33348376ffe8e56edadc29e20da4e7dc
Shashwat Shekhar <svs6603@psu.edu>
2020-10-12 21:12:00 +0530

    Find magic order

--- assignment3/problem1/solution.py
+++ assignment3/problem1/solution.py
@@ -1,6 +1,58 @@
-def DFS(reverse_edge_array):
+clock = 0
+pre = []
+post = []
+postlist = []
+visited = []
+vertex = []
+
+def explore (num_vertices, num_edges, edge_array, i, visit_vertex):
+  global clock
+  count = 0
+
+  print(f"Clock: {clock}")
+  print(clock)
+  pre.append(clock)
+
+  clock += 1
+  visited[i] = 1
+
+  print(visited)
+
+  for edge in edge_array:
+    print(f"Visit vertex in explore {visit_vertex}")
+    if(edge[0] == visit_vertex):
+      vertex_j = edge[1]
+      count += 1
+
+  if (count != 0): 
+    for edge in edge_array:
+      if(visited[vertex.index(vertex_j)] == 0):
+        print(f"Vertex j: {vertex_j}")
+        explore (num_vertices, num_edges, edge_array, vertex.index(vertex_j), vertex_j)
+
+  post.insert(i, clock)
+  clock += 1
+  postlist.append(visit_vertex)
+
+  print(f"Postlist: {postlist}")
 
   return
+
+
+def DFS(num_vertices, num_edges, edge_array):
+
+  global clock
+  clock = 1
+
+  print(f"Number of vertices in DFS: {num_vertices}")
+  for i in range(num_vertices):
+    vertex.append(i+1) 
+    visited.append(0)
+  
+  for i in range(num_vertices):
+    if(visited[i] == 0):
+      print(f"Enter explore to explore {vertex[i]}")
+      explore(num_vertices, num_edges, edge_array, i, vertex[i])
 
 def magic_order(num_vertices, num_edges, edge_array):
 
@@ -13,8 +65,8 @@ def magic_order(num_vertices, num_edges, edge_array):
 
   print(f"Reversed order of edges: {reverse_edge_array}")
 
-  DFS(reverse_edge_array)
-  
+  DFS(num_vertices, num_edges, reverse_edge_array)
+
   return
 
 def main():
