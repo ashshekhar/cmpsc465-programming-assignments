84f4241b867a39fb2b094f1954f38172230e5b5f
Shashwat Shekhar <svs6603@psu.edu>
2020-10-12 19:35:20 +0530

    Create reverse graph

--- assignment3/problem1/solution.py
+++ assignment3/problem1/solution.py
@@ -1,3 +1,22 @@
+def DFS(reverse_edge_array):
+
+  return
+
+def magic_order(num_vertices, num_edges, edge_array):
+
+  reverse_edge_array = []
+  
+  for edge in edge_array:
+    orig_u = edge[0]
+    orig_v = edge[1]
+    reverse_edge_array.append([orig_v, orig_u])
+
+  print(f"Reversed order of edges: {reverse_edge_array}")
+
+  DFS(reverse_edge_array)
+  
+  return
+
 def main():
 
   line_1 = input()
@@ -12,6 +31,10 @@ def main():
     edge_input = input()
     edges = list(map(int, edge_input.strip().split(' ')))
     edge_array.append(edges)
+  
+  print(f"Original: {edge_array}")
+
+  magic_order(num_vertices, num_edges, edge_array)
   
   print(f"Number of vertices: {num_vertices}")
   print(f"Number of edges:{num_edges}")
