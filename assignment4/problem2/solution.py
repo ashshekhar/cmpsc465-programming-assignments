93032e03f4863369c222bd2a75fdb43ca3c20614
Shashwat Shekhar <svs6603@psu.edu>
2020-11-01 04:11:01 +0530

    Pass 10/10 test cases

---
+++ assignment4/problem2/solution.py
@@ -0,0 +1,24 @@
+# The elaborate thought process behind this code is explained in our groups' Writing Assignment 4, Problem 2.
+
+def LPS (A, B):
+
+  # Initializing the DP table with base condition
+  LPS_DP = [[0]*(len(B)+1) for i in range(len(A)+1)]
+
+  
+  for i in range(1, len(A)+1):
+    for j in range(1, len(B)+1):
+
+      # Starting from box [1][1]
+      if(A[i-1] == B[j-1]):
+        LPS_DP [i][j] = 1 + LPS_DP[i-1][j-1]
+        
+      else:
+        LPS_DP [i][j] = max(LPS_DP [i-1][j], LPS_DP [i][j-1])
+
+  print(LPS_DP[(len(A))][len(B)])
+
+line_1 = input()
+
+# Longest palindromic subsequence is the longest common subsequence between string A and reverse of string A
+LPS(line_1, line_1[::-1])
