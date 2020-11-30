236eec7191ba04340dfce17a6aed4de9d942d2c4
Shashwat Shekhar <svs6603@psu.edu>
2020-11-30 21:41:09 +0530

    Update solution

--- assignment5/problem1/solution.py
+++ assignment5/problem1/solution.py
@@ -1,25 +1,31 @@
 def binary_search(interval_list, k, a, b):
   m = (a+b)//2
 
-  if(a == b):
-    return a
+  if(a <= b):
+    if(a == b):
+      if(interval_list[k][0] < interval_list[a][1]):
+        return 0
 
-  if((interval_list[m][1] <= interval_list[k][0]) and (interval_list[k][0] <= interval_list[m+1][1])):
-    return m
-  
-  elif(interval_list[m][1] > interval_list[k][0]):
-    return binary_search(interval_list, k, a, m)
+    if((interval_list[m][1] <= interval_list[k][0]) and (interval_list[k][0] <= interval_list[m+1][1])):
+      print("First if")
+      return m
+    
+    if(interval_list[m][1] > interval_list[k][0]):
+      print("Second if")
+      return binary_search(interval_list, k, a, m)
 
-  elif(interval_list[k][0] > interval_list[m+1][1]):
-    return binary_search(interval_list, k, m+1, b)
+    if(interval_list[k][0] > interval_list[m+1][1]):
+      print("Third if")
+      return binary_search(interval_list, k, m+1, b)
 
   else:
+    print("Final else")
     return 0
 
 def weighted_interval_scheduling(interval_list):
 
   for k in range(1, num_jobs):
-    pre[k] = binary_search(interval_list, k, 0, k-1)
+    pre[k] = binary_search(interval_list, k, 1, k-1)
     F[k] = max(F[k-1], F[pre[k]]+interval_list[k][2])
 
   return F[num_jobs-1]
