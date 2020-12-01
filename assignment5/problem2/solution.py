2ce58dd57aad559c19202529f39553a718911415
Shashwat Shekhar <svs6603@psu.edu>
2020-12-01 10:46:57 +0530

    Update solution.py

--- assignment5/problem2/solution.py
+++ assignment5/problem2/solution.py
@@ -1,62 +1,54 @@
-from collections import defaultdict
+from collections import namedtuple
 
 class Node:
   def __init__(self, data, parent, height):
     self.data = data
-    self.parent = parent
+    self.parent = self
     self.height = height
 
+  def make_set(self):
+    self.height = 1
+    self.parent = self
 
-def make_set(x):
-  x.height = 1
-  x.parent = x
+  def find(self):
+    while(self.parent != self):
+      self = self.parent
+    return self
 
-def find(x):
-  while(x.parent != x):
-    x = x.parent
-  return x
+  def union(self, other):
+    r_x = find(self)
+    r_y = find(other)
 
-def union(x, y):
-  r_x = find(x)
-  r_y = find(y)
+    if r_x == r_y:
+      return
+    
+    if(r_x.height > r_y.height):
+      r_y.parent = r_x
 
-  if r_x == r_y:
-    return
-  
-  if(r_x.height > r_y.height):
-    r_y.parent = r_x
+    elif(r_x.height < r_y.height):
+      r_x.parent = r_y
 
-  elif(r_x.height < r_y.height):
-    r_x.parent = r_y
-
-  else:
-    r_y.parent = r_x
-    r_x += 1
+    else:
+      r_y.parent = r_x
+      r_x += 1
 
 
-def kruskals(interval_list):
-
-  for item in interval_list:
-    print(item)
-
-  for _ in range(num_vertices):
-    make_set(Node())
-
+# def Kruskals(interval_list):
 
 user_input = input().strip().split(" ")
 num_vertices = int(user_input[0])
 num_edges = int(user_input[1])
 
-graph = defaultdict(dict)
+edges_list = []
 
 for edge in range(num_edges):
   edge_input = input()
   edges = list(map(int, edge_input.strip().split(' ')))
-  graph[edges[0]] [edges[1]] = edges[2]
+  edges_list.append(edges)
 
-# Sort the graph by edge values
+edges_list.sort(key=lambda x:x[2])
 
 print(num_vertices)
 print(num_edges)
-print(graph)
-# print(kruskals(graph))
+print(edges_list)
+# print(Kruskals(edges_list))
